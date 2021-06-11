e18f1bd73fb5be76d61329374acea2b4f991af3a
antikleya <48888148+antikleya@users.noreply.github.com>
2021-06-11 11:20:03 +0300

    Moved return codes to core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,5 @@
 from .degub_functions import write_dict
 from .formatting_functions import space_delete, get_str_table_struct
-from .constants import return_codes
 from .save_functions import save_row, save_options, create_table
+
+return_codes = {'OK': 200, 'Error': '400'}
--- core/constants.py
+++
@@ -1,1 +0,0 @@
-return_codes = {'OK': 200, 'Error': '400'}
