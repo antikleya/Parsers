b15da6ce0b83429271c2869d51c883fe25c56710
antikleya <48888148+antikleya@users.noreply.github.com>
2021-06-15 02:58:31 +0300

    a

--- settings/header_settings.py
+++ settings/header_settings.py
@@ -19,3 +19,14 @@ Ozon_headers = {
     'Upgrade-Insecure-Requests': '1',
     'Connection': 'keep-alive'
 }
+
+ChitaiGorod_headers = {
+    'Host': 'www.chitai-gorod.ru',
+    'Accept': '*/*',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.chitai-gorod.ru/',
+    'Cookie': 'visid_incap_229783=MAYgOBY5R1uUDN/nZoBS+vHex2AAAAAAQUIPAAAAAACTYZEPHzugj7rV0GCjL1Gm; incap_ses_580_229783=zct3cFl99htqsf/czJMMCPHex2AAAAAAFURpLMn1/1x4E8rnqcpc3g==; nlbi_229783=gante4Dp12tXSpUQW9nY4QAAAABs75NM91vSNeas8ypIj6iX; nlbi_229783_2147483646=ypcdGqS42juRWtOSW9nY4QAAAABPH0b3DveGhfrEfy6/yUY7; PHPSESSID=t0u14bvu8vtuh55jjnm4nbgs2k; cguuid=1623711480_k0lkbc5136cdqppdvmrdqnje0h; chg_ref=https%3A%2F%2Fduckduckgo.com%2F; chg_req=https%3A%2F%2Fwww.chitai-gorod.ru%2F; cityId=213; cityName=%CC%EE%F1%EA%E2%E0; gdeslon.ru.__arc_domain=gdeslon.ru; gdeslon.ru.user_id=0d9b3432-4af8-477a-8cb1-54953f171a48; _ym_uid=1623711480535606622; _ym_d=1623711480; _fbp=fb.1.1623711480094.723679882; _gcl_au=1.1.448912856.1623711480; adid=162371148089095; tmr_reqNum=3; tmr_lvid=f59b354429e0351f0039f74f84bea420; tmr_lvidTS=1623711481112; _ym_isad=2; _ga=GA1.2.199931850.1623711481; _gid=GA1.2.1391765689.1623711481; cto_bundle=mflNKF9nRG5pREFzMSUyQjdnMGJMTzFxNzViT1g2V3ZFbW1jbkxBRlZxOE92dGc0Nk5rYmZjRVBIZnVFdkx2dXpMUFlMQzlreW81NnFUbWd3ZXE1ejlLWXg4bldHRTVxNWJhSm1ja2puOVp0c3l3WUZNUVlyTTViamgyJTJCU0hIb0trbjBnd0JtQVU3SmhoOUtUUE9iSVpqTW1iaGV3JTNEJTNE; tmr_detect=0%7C1623711483721; visid_incap_2439285=DqV9R80dTamGcXliUnl1Ginfx2AAAAAAQUIPAAAAAADo3fnnmYpITdXgXRjTBv8G; nlbi_2439285=e4lBDCHY1HpbI/BEUHlpUgAAAABx+Zj3xV5z7LE3QpTS5APp; incap_ses_1317_2439285=TXInJ9isFSGvqfmD2exGEirfx2AAAAAAXewKu9gSENyvY/1InhDLrA==; showed_popup_21=1; reese84=3:mDRLOTpazOkG0PF172tQvw==:GJ3vdtL6hn8z8p1pkPUkjzFZmEjm2Pi+/j5+rUrk7F2CMufpXDKROr4Xn/akPIbguMywRUoNG/3RcBjmElUHduO9v0XxKQ6wPqRVm8R9YDKHX9jEOS4J6bxJXCQW2rieoscs95OUgkf6SCejU+P21rFD8+kg3aCyLC0iSL9U788Fbqd4G5DNcJwOgA3BY4GUZCpRj0mZaGQEfy8bxojxsPNhD1LGnh4ar4+RoaS3Wwg7ZFhrDZ93AWxTnPHTNo+wbIuJ8zuCgUCYU6RqaZ1heNk80fQhnBo5MktuGSmBG+/kc4+fhSAbM7glHeLb8FyKHmSyE20nCCNnfhHl4mzaEYrLda3d8W57f0bRkO+gi3o4fOYpqMeNLiy6jbGDwk7Q9qrwgKsCoayUZ8UPKR88PKoDgq+g81ciefZx/v1T8ZA=:KT2mHcX0LhSVEYDwvtB6LCceOveurKdw2IN8KEzHyZg=',
+    'TE': 'Trailers'
+}
---
+++ test.py
@@ -0,0 +1,16 @@
+import nordvpn_switcher as ns
+from random_user_agent.user_agent import UserAgent
+from settings import ChitaiGorod_headers
+from DetMir import get_json_data
+from core import write_dict, get_new_headers, get_new_session, user_agent_rotator
+from bs4 import BeautifulSoup
+import cfscrape
+
+
+target_url = "https://market.yandex.ru/catalog--mobilnye-telefony/54726/list?hid=91491&cpa=0&onstock=1&local-offers-first=0"   # replace url with anti-bot protected website
+scraper = cfscrape.create_scraper()
+html_text = scraper.get(target_url).text
+parsed_html = BeautifulSoup(html_text, 'html.parser')
+with open('output.txt', 'w') as file:
+    file.write(parsed_html.prettify())
+
